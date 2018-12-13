13d2d931ee3459964223d85153a94cfb981f7f27
Gregor <gregorberger1997@gmail.com>
2018-12-13 17:47:27 +0100

    - Naredu dnevne dogodke, - Spremenu barvo pri kontaktu

--- app/src/main/java/com/example/gregor/festhelp/MainActivity.java
+++ app/src/main/java/com/example/gregor/festhelp/MainActivity.java
@@ -12,23 +12,61 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_support);
 
 
-        ((Button) findViewById(R.id.kontakt)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent nextActivityIntent = new Intent(getBaseContext(), SupportActivity.class);
-                startActivity(nextActivityIntent);
-
-
-
-            }
-        });
+//        ((Button) findViewById(R.id.kontakt)).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                setContentView(R.layout.activity_support);
+//
+//
+//                String to = "festhelpsupport@gmail.com";
+//                String sub = ((EditText)findViewById(R.id.kontaktEmailNaslov)).getText().toString();
+//                String mess = ((EditText)findViewById(R.id.kontaktSporocilo)).getText().toString();
+//                Intent mail = new Intent(Intent.ACTION_SEND);
+//                mail.putExtra(Intent.EXTRA_EMAIL,new String[]{to});
+//                mail.putExtra(Intent.EXTRA_SUBJECT, sub);
+//                mail.putExtra(Intent.EXTRA_TEXT, mess);
+//                mail.setType("message/rfc822");
+//                startActivity(Intent.createChooser(mail, "Send email via:"));
+//
+//
+//
+//            }
+//        });
 
 
 
     }
+
+//    @Override
+//    public void onBackPressed() {
+//        setContentView(R.layout.activity_main);
+//
+//
+//        ((Button) findViewById(R.id.kontakt)).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                setContentView(R.layout.activity_support);
+//
+//                String to = "festhelpsupport@gmail.com";
+//                String sub = ((EditText)findViewById(R.id.kontaktEmailNaslov)).getText().toString();
+//                String mess = ((EditText)findViewById(R.id.kontaktSporocilo)).getText().toString();
+//                Intent mail = new Intent(Intent.ACTION_SEND);
+//                mail.putExtra(Intent.EXTRA_EMAIL,new String[]{to});
+//                mail.putExtra(Intent.EXTRA_SUBJECT, sub);
+//                mail.putExtra(Intent.EXTRA_TEXT, mess);
+//                mail.setType("message/rfc822");
+//                startActivity(Intent.createChooser(mail, "Send email via:"));
+//
+//
+//
+//            }
+//        });
+
+
+//    }
 
 
 }
