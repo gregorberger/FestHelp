9a664b4de3c2ebe7ac8dd1d5809176520e13c69c
gapus97 <gaspersmerkolj@gmail.com>
2018-12-13 16:05:52 +0100

    database

--- app/src/main/java/com/example/gregor/festhelp/MainActivity.java
+++ app/src/main/java/com/example/gregor/festhelp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.gregor.festhelp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
 
+    private Button button;
+    Button show_t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb=new DatabaseHelper(this);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
     }
+    public void openActivity2()
+    {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+
 }
