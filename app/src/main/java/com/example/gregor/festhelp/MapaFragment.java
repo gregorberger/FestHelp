dbb7b10ddc27ed44048680cc863f9290247ddd9c
Gregor <gregorberger1997@gmail.com>
2019-06-10 14:39:26 +0200

    - Added Map

--- app/src/main/java/com/example/gregor/festhelp/MapaFragment.java
+++ app/src/main/java/com/example/gregor/festhelp/MapaFragment.java
@@ -1,5 +1,6 @@
 package com.example.gregor.festhelp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.github.chrisbanes.photoview.PhotoView;
 
 
 public class MapaFragment extends Fragment {
@@ -16,6 +21,18 @@ public class MapaFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_mapa, container, false);
+        PhotoView photoView = (PhotoView) v.findViewById(R.id.mapaImageView);
+        photoView.setImageResource(R.drawable.mapa);
+
+//        Button glavniOder = (Button) v.findViewById(R.id.buttonMapaGlavniOder);
+//
+//        glavniOder.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent in = new Intent(getActivity(), NocniActivity.class);
+//                startActivity(in);
+//            }
+//        });
 
         return v;
     }
